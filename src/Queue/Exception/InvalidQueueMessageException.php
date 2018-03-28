54d4c3a7d000abdb67485d52c0ee6aed1e942040
frank <frank@qlimix.net>
2018-03-28 20:38:32 +0200

    Pass message id for acknowledging & queueMessage for logging and insights

--- src/Queue/Exception/InvalidQueueMessageException.php
+++ src/Queue/Exception/InvalidQueueMessageException.php
@@ -4,4 +4,38 @@ namespace Qlimix\Queue\Queue\Exception;
 
 final class InvalidQueueMessageException extends \Exception
 {
+    /** @var string */
+    private $messageId;
+
+    /** @var string */
+    private $queueMessage;
+
+    /**
+     * @param string $message
+     * @param \Exception $previous
+     * @param string $messageId
+     * @param string $queueMessage
+     */
+    public function __construct(string $message, \Exception $previous, string $messageId, string $queueMessage)
+    {
+        parent::__construct($message, 0, $previous);
+        $this->messageId = $messageId;
+        $this->queueMessage = $queueMessage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessageId(): string
+    {
+        return $this->messageId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQueueMessage(): string
+    {
+        return $this->queueMessage;
+    }
 }
