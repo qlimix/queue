2d8ee6b430ecb4eb6ca453c1cf2ef9b8155f32e3
frank <frank@qlimix.net>
2018-09-01 15:52:44 +0200

    consume a x amount of message from a queue

--- src/Consumer/QueueConsumerInterface.php
+++ src/Consumer/QueueConsumerInterface.php
@@ -3,11 +3,16 @@
 namespace Qlimix\Queue\Consumer;
 
 use Qlimix\Queue\Consumer\Exception\ConsumerException;
+use Qlimix\Queue\Queue\QueueMessage;
 
 interface QueueConsumerInterface
 {
     /**
+     * @param int $amount
+     *
+     * @return QueueMessage[]
+     *
      * @throws ConsumerException
      */
-    public function consume(): void;
+    public function consume(int $amount): array;
 }
