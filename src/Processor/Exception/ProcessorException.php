06a9be8fd61fd8fed08e3b3bf123f2ff95f19130
frank <frank@qlimix.net>
2018-09-02 17:08:46 +0200

    throw exception if can't process

---
+++ src/Processor/Exception/ProcessorException.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Qlimix\Queue\Processor\Exception;
+
+final class ProcessorException extends \Exception
+{
+}
--- src/Processor/ProcessorInterface.php
+++ src/Processor/ProcessorInterface.php
@@ -2,12 +2,15 @@
 
 namespace Qlimix\Queue\Processor;
 
+use Qlimix\Queue\Processor\Exception\ProcessorException;
 use Qlimix\Queue\Queue\QueueMessage;
 
 interface ProcessorInterface
 {
     /**
      * @param QueueMessage $message
+     *
+     * @throws ProcessorException
      */
     public function process(QueueMessage $message): void;
 }
