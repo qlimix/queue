39d51ddd3eb6466aaed71f9ef3e8fa5324afdd15
frank <frank@qlimix.net>
2019-01-14 20:35:55 +0100

    dispatch interface throws exception

--- src/Dispatcher/DispatcherInterface.php
+++ src/Dispatcher/DispatcherInterface.php
@@ -2,10 +2,14 @@
 
 namespace Qlimix\Queue\Dispatcher;
 
+use Qlimix\Queue\Dispatcher\Exception\DispatchException;
+
 interface DispatcherInterface
 {
     /**
      * @param mixed $message
+     *
+     * @throws DispatchException
      */
     public function dispatch($message): void;
 }
